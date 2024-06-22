62d82e3a6b0ff121135cade0977d4500ef79ab43
Kasper JH. Alfarnes <49387825+kasperjha@users.noreply.github.com>
2024-06-22 13:41:50 +0200

    refactor: inline svgs

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,19 @@
 import './index.css';
 
-const drawings = [
-  '/drawing-1.svg',
-  '/drawing-2.svg',
-  '/drawing-3.svg',
-  '/drawing-4.svg',
-]
+import kalfOne from '/drawing-1.svg?raw';
+import kalfTwo from '/drawing-2.svg?raw';
+import kalfThree from '/drawing-3.svg?raw';
+import kalfFour from '/drawing-4.svg?raw';
+
+const drawings = [kalfOne, kalfTwo, kalfThree, kalfFour];
+
+function loadRandomDrawing() {
+  const drawingContainer = document.getElementById('kalf-drawing') as HTMLDivElement;
+  const randomDrawing = drawings[Math.floor(Math.random() * drawings.length)];
+  drawingContainer.innerHTML = randomDrawing;
+}
 
 const newButton = document.getElementById('kalf-new') as HTMLButtonElement;
-const drawing = document.getElementById('kalf-drawing') as HTMLImageElement;
+newButton.addEventListener('click', loadRandomDrawing)
+loadRandomDrawing()
 
-let idx = Math.floor(Math.random() * drawings.length);
-
-drawing.src = drawings[idx];
-
-newButton?.addEventListener('click', () => {
-  idx = (idx + 1) % drawings.length;
-  drawing.src = drawings[idx];
-});
