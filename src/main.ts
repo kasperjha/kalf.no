27c804e6ff1b102f80a44af8d090a8588a02e9bb
Kasper JH. Alfarnes <49387825+kasperjha@users.noreply.github.com>
2024-06-23 23:33:42 +0200

    add: dana's kalf

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,9 @@ import kalfOne from '/drawing-1.svg?raw';
 import kalfTwo from '/drawing-2.svg?raw';
 import kalfThree from '/drawing-3.svg?raw';
 import kalfFour from '/drawing-4.svg?raw';
+import kalfDana from '/dana.svg?raw';
 
-const drawings = [kalfOne, kalfTwo, kalfThree, kalfFour];
+const drawings = [kalfOne, kalfTwo, kalfThree, kalfFour, kalfDana];
 const drawingContainer = document.getElementById('kalf-drawing') as HTMLDivElement;
 const newButton = document.getElementById('kalf-new') as HTMLButtonElement;
 
