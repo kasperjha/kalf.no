44c1615214d50aecc8b06f20cb5dfff33112eff6
Kasper JH. Alfarnes <49387825+kasperjha@users.noreply.github.com>
2025-08-05 11:34:04 +0200

    feat: add basic seo tags

--- app/app.vue
+++ app/app.vue
@@ -5,6 +5,14 @@
 </template>
 
 <script lang="ts" setup>
+
+useSeoMeta({
+  'title': 'kalf.no',
+  description: 'collection of single line drawings curated by kasper alfarnes',
+  ogTitle: 'kalf.no',
+  ogDescription: 'collection of single line drawings curated by kasper alfarnes',
+})
+
 useHead({
   script: [
     { defer: true, src: 'https://plausible.kakk.dev/js/script.js', 'data-domain': 'kalf.no' }
