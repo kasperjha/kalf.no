13d7224bdb93cd4b10af3641cca93db8d846b36a
Kasper JH. Alfarnes <49387825+kasperjha@users.noreply.github.com>
2025-09-11 21:59:26 +0200

    chore: add eirik kalf

--- app/drawings.ts
+++ app/drawings.ts
@@ -25,6 +25,7 @@ import kalfVetle from '~/assets/vetle.svg?raw';
 import kalfEli from '~/assets/eli.svg?raw';
 import kalfMarijke from '~/assets/marijke.svg?raw';
 import kalfMarie from '~/assets/marie.svg?raw';
+import kalfEirik from '~/assets/eirik.svg?raw';
 
 import type { KalfSubmission } from './types/KalfSubmission';
 
@@ -57,4 +58,5 @@ export const drawings: KalfSubmission[] = [
   { svg: kalfEli, credit: { by: 'ealfa', date: '2025-08-05'} },
   { svg: kalfMarijke, credit: { by: '@marijkehopen',  byLink: 'https://www.instagram.com/marijkehopen', date: '2025-08-06'} },
   { svg: kalfMarie, credit: { by: '@marie.hano',  byLink: 'https://www.instagram.com/marie.hano', date: '2025-08-08'} },
+  { svg: kalfEirik, credit: { by: '@eagrafikk',  byLink: 'https://www.instagram.com/eagrafikk', date: '2025-09-11'} },
 ];
